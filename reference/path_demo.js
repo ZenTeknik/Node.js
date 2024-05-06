868f5f06aabd5a2da40fe198175f14e519b6c213
haris7866 <arainharis151@gmail.com>
2024-05-06 17:56:27 +0500

    Update

---
+++ reference/path_demo.js
@@ -0,0 +1,18 @@
+const path=require('path');
+
+// basename
+console.log(path.basename(__filename));
+
+// directory name 
+console.log(path.dirname(__dirname));
+
+
+// file extension  like c++,c, or js etc
+console.log(path.extname(__filename));
+
+
+// create path object 
+console.log(path.parse(__filename).base);
+
+// concatenate path
+console.log(path.join(__dirname,'teste','hello.html'));
