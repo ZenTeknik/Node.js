6007a07043b0935773ba621a85ac31a6aaecf620
haris7866 <arainharis151@gmail.com>
2024-05-06 18:19:58 +0500

    New

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,12 +7,30 @@ const path = require('path');
     if (err) throw err;
     console.log("Folder created");
 });
-*/
+
 
 // Write file 
-fs.writeFile(path.join(__dirname, './test', 'hello.txt'), 'hello world', (err) => {
+fs.writeFile
+(path.join(__dirname, './test', 'hello.txt'), 'hello world', (err) => {
     if (err) throw err;
     console.log("File written");
 });
 
+
+// append th file
+fs.appendFile
+(path.join(__dirname, './test', 'hello.txt'), '   I love nodjs', (err) => {
+    if (err) throw err;
+    console.log("File append");
+});
+
+
+
+*/
+
+// read the file
+fs.readFile(path.join(__dirname, './test','hello.txt'), 'utf8', (err,data) => {
+    if (err) throw err;
+    console.log(data);
+});
 
